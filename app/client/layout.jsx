9b69c0ea3fa9686ac92de39fc04f1443d505f86a
Camilo <camilomedina.es@gmail.com>
2025-01-31 23:04:52 -0300

    se quitan opciones de login de cliente, se refactoriza un componente en panel de administrador

--- app/client/layout.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useAuthContext } from "../context/AuthContext";
-
-export default function ClientLayout({ children, login }) {
-  const { user } = useAuthContext();
-  return <>{user ? children : login}</>;
-}
--- components/layouts/adminDashboard/ProductEditCard.jsx
+++ components/layouts/adminDashboard/ProductEditCard.jsx
@@ -51,12 +51,13 @@ const ProductEditCard = ({ item }) => {
     reset(item);
   };
   return (
-    <section className="flex flex-row justify-center bg-base-300">
-      <article className="card card-compact lg:card-side lg:w-full xl:w-5/6 bg-base-100 shadow-xl my-3">
+    <section className="flex flex-col justify-self-center">
+      <h2 className="text-2xl font-bold text-center mt-1">Panel de edición</h2>
+      <article className="card w-96 card-compact lg:card-side lg:w-full xl:w-5/6 bg-base-100 shadow-xl my-3">
         <div>
           <Link
             href={`/admin`}
-            className="btn btn-square absolute btn-primary right-px"
+            className="btn btn-square  btn-primary absolute right-px top-px"
           >
             Volver
           </Link>
